933bb8978cf912c24e2aab4d9ee68db3f4948340
chuntm <yuand6546@gmail.com>
2023-09-20 08:59:44 +0700

    ss7

---
+++ src/Entity/MyClass.java
@@ -0,0 +1,29 @@
+package Entity;
+public class MyClass {
+    private String myString;
+
+    public MyClass() {
+    }
+
+    public MyClass(String myString) {
+        this.myString = myString;
+    }
+
+    public static void main(String[] args) {
+        MyClass myObject = new MyClass();
+
+        System.out.println("Giá trị ban đầu của myString: " + myObject.getMyString());
+
+        myObject.setMyString("Hello, world!");
+
+        System.out.println("Giá trị mới của myString: " + myObject.getMyString());
+    }
+
+    public String getMyString() {
+        return myString;
+    }
+
+    public void setMyString(String myString) {
+        this.myString = myString;
+    }
+}
