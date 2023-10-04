286a7cc83ef180bc6bc497b8be321a2aecedf0fe
chuntm <yuand6546@gmail.com>
2023-10-04 14:04:38 +0700

    m

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+import com.sun.source.tree.Tree;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        HashMap<Student, Integer> hashMap = new HashMap<>();
+        Student s1 = new Student("Hùng"); // thêm mới và chỉnh sửa
+        hashMap.put(new Student("Nam"), 5); // thêm mới và chin sửa
+        TreeMap<Student, Integer> treeMap = new TreeMap<>();
+        treeMap.put(null, 12);
     }
 }
